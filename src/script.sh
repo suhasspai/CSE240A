195eefc2dd02a7ef900fab29cbfe16e0a07da647
Subhashree Navaneethan <n.subhasree@yahoo.in>
2022-12-05 00:52:08 -0800

    run all traces

--- src/script.sh
+++ src/script.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 make clean
 make
-bunzip2 -kc ../traces/int_1.bz2 | ./predictor --gshare:32
-bunzip2 -kc ../traces/int_1.bz2 | ./predictor --tournament:32:16:16
+echo "running int_1.bz2 trace..."
+bunzip2 -kc ../traces/int_1.bz2 | ./predictor --gshare:13
+bunzip2 -kc ../traces/int_1.bz2 | ./predictor --tournament:9:10:10
+echo "running int_2.bz2 trace..."
+bunzip2 -kc ../traces/int_2.bz2 | ./predictor --gshare:13
+bunzip2 -kc ../traces/int_2.bz2 | ./predictor --tournament:9:10:10
+echo "running fp_1.bz2 trace..."
+bunzip2 -kc ../traces/fp_1.bz2 | ./predictor --gshare:13
+bunzip2 -kc ../traces/fp_1.bz2 | ./predictor --tournament:9:10:10
+echo "running fp_2.bz2 trace..."
+bunzip2 -kc ../traces/fp_2.bz2 | ./predictor --gshare:13
+bunzip2 -kc ../traces/fp_2.bz2 | ./predictor --tournament:9:10:10
+echo "running mm_1.bz2 trace..."
+bunzip2 -kc ../traces/mm_1.bz2 | ./predictor --gshare:13
+bunzip2 -kc ../traces/mm_1.bz2 | ./predictor --tournament:9:10:10
+echo "running mm_2.bz2 trace..."
+bunzip2 -kc ../traces/mm_2.bz2 | ./predictor --gshare:13
+bunzip2 -kc ../traces/mm_2.bz2 | ./predictor --tournament:9:10:10
